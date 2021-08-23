ede9e1928e51fd9aead5757e876c10f538fb51f5
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-08-23 11:37:13 -0700

    Created a timer component to keep track of seconds passed

---
+++ src/Board/Timer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import './Timer.css';
+library.add(faClock); 
+
+export default function Timer(props) {
+  return(
+    <div className="timer">
+      <FontAwesomeIcon className="faClock" size="md" icon={faClock}/>
+      <p><strong>{props.time}</strong></p>
+    </div>
+  )
+}
