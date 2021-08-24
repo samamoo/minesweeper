67693b64f3b5f708317fafc29f42ab2cd1fccec0
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-08-24 13:30:14 -0700

    Changed BombCounter to FlagCounter

--- src/Board/FlagCounter.js
+++ src/Board/FlagCounter.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFlag } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import './BombCounter.css';
+import './FlagCounter.css';
 library.add(faFlag); 
 
-export default function BombCounter(props) {
+export default function FlagCounter(props) {
   return(
     <div className="counter">
-      <FontAwesomeIcon className="faFlag" size="md" icon={faFlag}/>
-        <p><strong>{props.bombCount}</strong></p>
+      <FontAwesomeIcon className="faFlag" size="1x" icon={faFlag}/>
+        <p><strong>{props.flagCount}</strong></p>
     </div>
   )
 }
