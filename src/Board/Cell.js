2f8fce3f93e5e146c101cd6737b7523ce3e8c163
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-08-25 11:43:44 -0700

    Installed classnames and set classes for selected and flagged cells

--- src/Board/Cell.js
+++ src/Board/Cell.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import classnames from 'classnames';
 import './Cell.css';
 
 export default function Cell(props) {
+
+  const cellClass = classnames("cell", {
+    "cell--selected": props.selected,
+    "cell--flagged": props.flagged,
+  });
+
+
   return(
-    <div className="cell">
+    <div className={cellClass} selected={props.selected} flagged={props.flagged} 
+    onClick={() => props.selectCell(props)}
+    onContextMenu={(e)=> props.flagCell(e, props)}>
       <p>{props.value}</p>
     </div>
   )
