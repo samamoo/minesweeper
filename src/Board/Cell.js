906d08543bd4d692ca9f39c4addaa8ed8f05e2d7
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-08-25 16:18:55 -0700

    Styled cells to display a flag/bomb/value on click

--- src/Board/Cell.js
+++ src/Board/Cell.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import classnames from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPoop, faFlag } from '@fortawesome/free-solid-svg-icons';
 import './Cell.css';
+library.add(faPoop,faFlag);
 
 export default function Cell(props) {
 
   const cellClass = classnames("cell", {
     "cell--selected": props.selected,
     "cell--flagged": props.flagged,
+    "cell--bomb":  props.value === "bomb" && props.gameOver,
   });
 
 
@@ -14,7 +19,13 @@ export default function Cell(props) {
     <div className={cellClass} selected={props.selected} flagged={props.flagged} 
     onClick={() => props.selectCell(props)}
     onContextMenu={(e)=> props.flagCell(e, props)}>
-      <p>{props.value}</p>
+      { props.value === "bomb" && props.gameOver && 
+      <FontAwesomeIcon size="lg" icon={faPoop}/>
+      }
+      {props.flagged && !props.gameOver &&
+      <FontAwesomeIcon size="lg" icon={faFlag}/>
+      }
+      {props.selected && props.value !== "bomb" && <p>{props.value}</p>}
     </div>
   )
 }
