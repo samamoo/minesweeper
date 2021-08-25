430a2f66b0faa94f32a03b48854606cfc9fc36a3
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-08-25 11:44:31 -0700

    Set logic to stop timer when bomb is clicked. and when a flag is set

--- src/Board/index.js
+++ src/Board/index.js
@@ -1,24 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 // Components
 import FlagCounter from './FlagCounter';
 import Timer from './Timer';
 import Cell from './Cell';
 // Helpers
-import { makeGrid } from '../helpers/helpers';
+import { makeGrid, checkEmpties } from '../helpers/helpers';
 // Styles
 import './index.css';
 
 export default function Board() {
   const [ time, setTime ] = useState(0);
-  const [ bombCount, setBombCount ] = useState(40);
+  const [ flagCount, setFlagCount ] = useState(40);
   const [ board, setBoard ] = useState([]); 
+  const [gameOver, setGameOver] = useState(false);
 
   // Start the timer on page load
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setTime((prev) => prev +1)
-  //   }, 1000);
-  // },[]);
+  let timer = useRef(null);
+  useEffect(() => {
+    if (!gameOver) {
+      startTimer();
+    }
+    return () => clearInterval(timer.current);
+  },[gameOver]);
+
+  const startTimer = () => {
+    timer.current = setInterval(() => {
+      setTime((prev) => prev +1)
+    }, 1000); 
+  }
 
   // Create a new board on page load
   useEffect(() => {
@@ -31,6 +40,46 @@ export default function Board() {
     setBoard(grid);
   }
 
+  console.log(gameOver);
+  // Select a Cell / On Left Click
+  const selectCell = (data) => {
+    if (data.value === "bomb") {
+      console.log("GAME OVER")
+      setGameOver(true);
+      // Stop the clock
+      // Trigger popup 
+    }
+    let updateState = board;
+    if (updateState[data.x][data.y].flagged === true) {
+      updateState[data.x][data.y].flagged = false;
+      setFlagCount(flagCount+1)
+    }
+    // Check if cell.value === 0 and reveal all adjacent 0 cells
+    if (updateState[data.x][data.y].value === 0) {
+      const newBoard = checkEmpties(board, data.x, data.y);
+      console.log(newBoard)
+    }
+
+    updateState[data.x][data.y].selected = true;
+    setBoard(updateState)
+  }
+  
+  // Flag a Cell / On Right Click
+  const flagCell = (e, data) => {
+    e.preventDefault();
+    console.log("Flagged")
+    let updateState = board;
+    if (updateState[data.x][data.y].flagged === true) {
+      updateState[data.x][data.y].flagged = false;
+      setFlagCount(flagCount+1)
+    } else {
+      updateState[data.x][data.y].flagged = true;
+      setFlagCount(flagCount-1)
+    }
+    setBoard(updateState)
+    console.log(board)
+  }
+
   if (!board) {
     return(
       <div>LOADING...</div>
@@ -40,10 +89,10 @@ export default function Board() {
   return( board &&
     <div className="board">
       <header className="board-header">
-        <FlagCounter bombCount={bombCount}/>
+        <FlagCounter flagCount={flagCount}/>
         <Timer time={time}/>
-        {/* Settings Component */}
         {/* Level Selection Component */}
+        {/* Settings Component */}
       </header>
 
       <div className="board-container">
@@ -57,6 +106,8 @@ export default function Board() {
                 flagged={value.flagged}
                 x={value.x}
                 y={value.y}
+                selectCell={selectCell}
+                flagCell={flagCell}
                 />
               )
             })
